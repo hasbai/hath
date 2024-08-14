f6a5179c9da7130f2f0947c6218d4f9deb807a57
hasbai <34417826+hasbai@users.noreply.github.com>
2024-08-14 19:54:37 +0800

    1

--- pages/monument.vue
+++ pages/monument.vue
@@ -25,7 +25,6 @@ async function init() {
 
 
   const vertices = new Float32Array([
-    //   X,    Y,
     -0.8, -0.8,
     0.8, -0.8,
     0.8, 0.8,
@@ -39,21 +38,32 @@ async function init() {
     size: vertices.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
   });
-  device.queue.writeBuffer(vertexBuffer, 0, vertices);
-  const vertexBufferLayout = {
+  device.queue.writeBuffer(vertexBuffer, 0, vertices)
+
+  const uniformArray = new Float32Array([GRID_SIZE, GRID_SIZE]);
+  const uniformBuffer = device.createBuffer({
+    label: "Grid Uniforms",
+    size: uniformArray.byteLength,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  });
+  device.queue.writeBuffer(uniformBuffer, 0, uniformArray);
+
+  const vertexBufferLayout: GPUVertexBufferLayout = {
     arrayStride: 8,
     attributes: [{
       format: "float32x2",
       offset: 0,
       shaderLocation: 0, // Position, see vertex shader
     }],
-  };
+  }
   const cellShaderModule = device.createShaderModule({
     label: "Cell shader",
     code: `
+    @group(0) @binding(0) var<uniform> grid: vec2f;
+
     @vertex
     fn vertexMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
-      return vec4f(pos, 0, 1);
+      return vec4f((pos + 1) / grid - 1, 0, 1);
     }
     @fragment
     fn fragmentMain() -> @location(0) vec4f {
@@ -77,6 +87,14 @@ async function init() {
       }]
     }
   })
+  const bindGroup = device.createBindGroup({
+    label: "Cell renderer bind group",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: { buffer: uniformBuffer }
+    }],
+  });
 
   const encoder = device.createCommandEncoder();
   const pass = encoder.beginRenderPass({
@@ -89,6 +107,7 @@ async function init() {
   })
   pass.setPipeline(cellPipeline)
   pass.setVertexBuffer(0, vertexBuffer)
+  pass.setBindGroup(0, bindGroup);
   pass.draw(vertices.length / 2); // 6 vertices
   pass.end()
   device.queue.submit([encoder.finish()]);
